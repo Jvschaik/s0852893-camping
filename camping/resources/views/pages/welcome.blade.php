eb9ebe8be027f8cad28b20a1a6eb39051381d812
Jason <jsonschaik@gmail.com>
2018-11-04 13:36:10 +0100

    hide/show on welcome page

--- camping/resources/views/pages/welcome.blade.php
+++ camping/resources/views/pages/welcome.blade.php
@@ -17,16 +17,18 @@
 <div class="container">
     <div class="row">
         @foreach($posts as $post)
-        <div class="col-lg-6">
-                <div class="card post mb-4" >
-                    {{--<img class="card-img-top" src="..." alt="Card image cap">--}}
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $post->title }}</h5>
-                        <p class="card-text">{{ substr($post->body, 0, 300)}}{{ strlen($post->body) > 300 ? "..." : "" }}</p>
-                        <a href="{{ url('post/'.$post->slug) }}" class="btn btn-outline-primary">Read more</a>
+            @if($post->visible)
+                <div class="col-lg-6">
+                    <div class="card post mb-4">
+                        {{--<img class="card-img-top" src="..." alt="Card image cap">--}}
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $post->title }}</h5>
+                            <p class="card-text">{{ substr($post->body, 0, 300)}}{{ strlen($post->body) > 300 ? "..." : "" }}</p>
+                            <a href="{{ url('post/'.$post->slug) }}" class="btn btn-outline-primary">Read more</a>
+                        </div>
                     </div>
                 </div>
-        </div>
+            @endif
         @endforeach
     </div>
 </div>
